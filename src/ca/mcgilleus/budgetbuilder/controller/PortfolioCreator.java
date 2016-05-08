dc5cdaa2a98ff765ff2a0f8b5927899ded955200
kareemhalabi <kareemhalabi24@gmail.com>
2016-05-07 21:33:14 -0400

    Portfolio styling done

--- src/ca/mcgilleus/budgetbuilder/controller/PortfolioCreator.java
+++ src/ca/mcgilleus/budgetbuilder/controller/PortfolioCreator.java
@@ -9,10 +9,12 @@ import java.util.Queue;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -31,6 +33,12 @@ public class PortfolioCreator {
 	private static Queue<IndexedColors> allColors;
 	private static Queue<IndexedColors> availableTabColors;
 	
+	private static final CellStyle HEADER_STYLE;
+	private static final CellStyle TEXT_CELL_STYLE;
+	private static final CellStyle CURRENCY_CELL_STYLE;
+	private static final CellStyle TOTAL_LABEL_STYLE;
+	private static final CellStyle TOTAL_CELL_STYLE;
+	
 	public static Queue<IndexedColors> getAllColors() {
 		return allColors;
 	}
@@ -44,6 +52,80 @@ public class PortfolioCreator {
 		availableTabColors = new LinkedList<IndexedColors>(allColors);
 	}
 	
+	//initialize styles
+	static {
+		HEADER_STYLE = EUSBudgetBuilder.getWorkbook().createCellStyle();
+		XSSFFont headerFont= EUSBudgetBuilder.getWorkbook().createFont();
+	    headerFont.setFontHeightInPoints((short)11);
+	    headerFont.setFontName("Arial");
+	    headerFont.setColor(IndexedColors.BLACK.getIndex());
+	    headerFont.setBold(true);
+	    headerFont.setItalic(false);
+	
+		HEADER_STYLE.setFont(headerFont);
+		
+		HEADER_STYLE.setWrapText(true);
+	    
+		HEADER_STYLE.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		HEADER_STYLE.setAlignment(CellStyle.ALIGN_CENTER);
+	
+		HEADER_STYLE.setBorderRight(CellStyle.BORDER_THIN);
+		HEADER_STYLE.setRightBorderColor(IndexedColors.BLACK.getIndex());
+		HEADER_STYLE.setBorderBottom(CellStyle.BORDER_THIN);
+		HEADER_STYLE.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+		
+		//----------------------------------------------------------------
+		
+		TEXT_CELL_STYLE = EUSBudgetBuilder.getWorkbook().createCellStyle();
+		XSSFFont basicFont= EUSBudgetBuilder.getWorkbook().createFont();
+	    basicFont.setFontHeightInPoints((short)10);
+	    basicFont.setFontName("Arial");
+	    basicFont.setColor(IndexedColors.BLACK.getIndex());
+	    basicFont.setBold(false);
+	    basicFont.setItalic(false);
+	    
+	    TEXT_CELL_STYLE.setFont(basicFont);
+	    
+	    TEXT_CELL_STYLE.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		TEXT_CELL_STYLE.setAlignment(CellStyle.ALIGN_CENTER);
+		
+		TEXT_CELL_STYLE.setBorderRight(CellStyle.BORDER_THIN);
+		TEXT_CELL_STYLE.setRightBorderColor(IndexedColors.BLACK.getIndex());
+		
+		//----------------------------------------------------------------
+		
+		CURRENCY_CELL_STYLE = EUSBudgetBuilder.getWorkbook().createCellStyle();
+		
+	    CURRENCY_CELL_STYLE.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		CURRENCY_CELL_STYLE.setAlignment(CellStyle.ALIGN_RIGHT);
+		
+		CURRENCY_CELL_STYLE.setBorderRight(CellStyle.BORDER_THIN);
+		CURRENCY_CELL_STYLE.setRightBorderColor(IndexedColors.BLACK.getIndex());
+		
+		CURRENCY_CELL_STYLE.setDataFormat((short) 8); // ($#,##0.00_);[Red]($#,##0.00)
+		
+		//----------------------------------------------------------------
+		
+		TOTAL_LABEL_STYLE = EUSBudgetBuilder.getWorkbook().createCellStyle();
+		
+		TOTAL_LABEL_STYLE.setFont(headerFont);
+		
+	    TOTAL_LABEL_STYLE.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		TOTAL_LABEL_STYLE.setAlignment(CellStyle.ALIGN_LEFT);
+		
+		//----------------------------------------------------------------
+		
+		TOTAL_CELL_STYLE = EUSBudgetBuilder.getWorkbook().createCellStyle();
+		
+	    TOTAL_CELL_STYLE.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		TOTAL_CELL_STYLE.setAlignment(CellStyle.ALIGN_RIGHT);
+		
+		TOTAL_CELL_STYLE.setBorderTop(CellStyle.BORDER_DOUBLE);
+		TOTAL_CELL_STYLE.setTopBorderColor(IndexedColors.BLACK.getIndex());
+		
+		TOTAL_CELL_STYLE.setDataFormat((short) 8); // ($#,##0.00_);[Red]($#,##0.00)
+	}
+
 	/**
 	 * Creates an EUS portfolio from a directory
 	 * @param f The root directory of the portfolio
@@ -71,11 +153,14 @@ System.out.println("Created portfolio: " + p.getName());
 		if (availableTabColors.isEmpty())
 			recreateColorQueue();
 
-		PortfolioCreator.createPortfolioOverView(p);
+		createPortfolioOverView(p);
 	}
 
-	//TODO styling
-	public static void createPortfolioOverView(Portfolio p) {
+	/**
+	 * Creates Portfolio Overview page showing amounts requested by portfolio's committees
+	 * @param p The porfilo overview to create
+	 */
+	private static void createPortfolioOverView(Portfolio p) {
 		
 		XSSFWorkbook wb = p.getEUSBudget().getWorkbook();
 		
@@ -94,7 +179,7 @@ System.out.println("Created portfolio: " + p.getName());
 		c.setTime(p.getEUSBudget().getYear());
 		String budgetYear = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.YEAR) +1);
 		
-		title.setCellValue("Budget " + budgetYear);
+		title.setCellValue("Super Duper Long Title Budget " + budgetYear);
 		
 		for(int i = 0; i < p.getCommitteeBudgets().size(); i++) {
 			CommitteeBudget committee = p.getCommitteeBudget(i);
@@ -124,8 +209,49 @@ System.out.println("Created portfolio: " + p.getName());
 			colTotal.setCellFormula(sumFormula);
 		}
 		
+		stylePortfolioOverview(overviewSheet);
+		
 System.out.println("\t Overview done!");		
 	}
 
-	
+	private static void stylePortfolioOverview(XSSFSheet overviewSheet) {
+		
+		XSSFRow header = overviewSheet.getRow(0);
+		
+		//Fix Column Widths
+		for(int i = 0; i < header.getLastCellNum(); i++) {
+			overviewSheet.autoSizeColumn(i);
+			int adjustedWidth = overviewSheet.getColumnWidth(i) + 256*3;
+			
+			if(adjustedWidth > 5000)
+				adjustedWidth = 5000;
+			
+			overviewSheet.setColumnWidth(i, adjustedWidth);
+		}
+		
+		//Apply Header styles
+		for(Cell cell : header) {
+			cell.setCellStyle(HEADER_STYLE);
+		}
+		
+		//Apply Data styles
+		for(int j = 1; j < overviewSheet.getLastRowNum()-1; j++) {
+			XSSFRow dataRow = overviewSheet.getRow(j);
+			
+			for(int k = 0; k < 2; k++)
+				dataRow.getCell(k).setCellStyle(TEXT_CELL_STYLE);
+			
+			
+			for(int k = 2; k < dataRow.getLastCellNum(); k++)
+				dataRow.getCell(k).setCellStyle(CURRENCY_CELL_STYLE);
+		}
+		
+		//Apply Total styles
+		XSSFRow totals= overviewSheet.getRow(overviewSheet.getLastRowNum());
+		XSSFCell totalLabel = totals.getCell(1);
+		totalLabel.setCellStyle(TOTAL_LABEL_STYLE);
+		
+		for(int l = 2; l < totals.getLastCellNum(); l++) 
+			totals.getCell(l).setCellStyle(TOTAL_CELL_STYLE);
+	}
 }
